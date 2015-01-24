d8281ec6665ac1661bc821e098533bccf49e5840
Andrea Biancini <andrea.biancini@gmail.com>
2015-01-24 10:02:46 +0100

    Added logout url

--- met/settings.py
+++ met/settings.py
@@ -162,6 +162,7 @@ LOGGING = {
 }
 
 LOGIN_URL = '%s/saml2/login' % BASEURL
+LOGOUT_URL = '%s/saml2/logout' % BASEURL
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',

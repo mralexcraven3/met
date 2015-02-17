36b3ccb2e35bfef11d0349151b27590c103ca01b
Andrea Biancini <andrea.biancini@gmail.com>
2015-02-17 17:28:51 +0100

    Added saml configuration

--- local_settings.example.py
+++ local_settings.example.py
@@ -51,6 +51,96 @@ ORGANIZATION_NAME = 'Your organization'
 
 SHIB_LOGOUT_URL = '/Shibboleth.sso/Logout'
 
+SAML2DIR = '/saml2'
+
+SAML_CONFIG = {
+  # full path to the xmlsec1 binary programm
+  'xmlsec_binary': '/usr/bin/xmlsec1',
+
+  # your entity id, usually your subdomain plus the url to the metadata view
+  'entityid': 'https://met-hostname.example.com/saml2/metadata/',
+
+  # directory with attribute mapping
+  'attribute_map_dir': os.path.join(SAML2DIR, 'attribute-maps'),
+
+  # this block states what services we provide
+  'service': {
+      # we are just a lonely SP
+      'sp': {
+          'name': 'Metadata Explorer Tool',
+          'endpoints': {
+              # url and binding to the assetion consumer service view
+              # do not change the binding or service name
+              'assertion_consumer_service': [
+                  ('https://met-hostname.example.com/met/saml2/acs/',
+                   saml2.BINDING_HTTP_POST),
+                  ],
+              # url and binding to the single logout service view
+              # do not change the binding or service name
+              'single_logout_service': [
+                  ('https://met-hostname.example.com/met/saml2/ls/',
+                   saml2.BINDING_HTTP_REDIRECT),
+                  ],
+              },
+          # # This is commented to be compatible with simplesamlphp
+          # # attributes that this project need to identify a user
+          #'required_attributes': ['mail'],
+          #
+          # # attributes that may be useful to have but not required
+          #'optional_attributes': ['eduPersonAffiliation'],
+
+          # in this section the list of IdPs we talk to are defined
+          'idp': {
+              # we do not need a WAYF service since there is
+              # only an IdP defined here. This IdP should be
+              # present in our metadata
+
+              # the keys of this dictionary are entity ids
+              'https://idp-hostname.example.com/idp/shibboleth': {
+                  'single_sign_on_service': {
+                      saml2.BINDING_HTTP_REDIRECT: 'https://idp-hostname.example.com/idp/profile/Shibboleth/SSO',
+                      },
+                  'single_logout_service': {
+                      saml2.BINDING_HTTP_REDIRECT: 'https://idp-hostname.example.com/idp/profile/Shibboleth/Logout',
+                      },
+                  },
+              },
+          },
+      },
+
+ # where the remote metadata is stored
+  'metadata': {
+      'local': [os.path.join(SAML2DIR, 'remote_metadata.xml')],
+      },
+
+  # set to 1 to output debugging information
+  'debug': 1,
+
+  # certificate
+  'key_file': os.path.join(SAML2DIR, 'certs/server.key'),  # private part
+  'cert_file': os.path.join(SAML2DIR, 'certs/server.crt'),  # public part
+
+  # own metadata settings
+  'contact_person': [
+      {'given_name': 'Sysadmin',
+       'sur_name': '',
+       'company': 'Example CO',
+       'email_address': 'sysadmin@example.com',
+       'contact_type': 'technical'},
+      {'given_name': 'Admin',
+       'sur_name': 'CEO',
+       'company': 'Example CO',
+       'email_address': 'admin@example.com',
+       'contact_type': 'administrative'},
+      ],
+  # you can set multilanguage information here
+  'organization': {
+      'name': [('Example CO', 'es'), ('Example CO', 'en')],
+      'display_name': [('Example', 'es'), ('Example', 'en')],
+      'url': [('http://www.example.com', 'es'), ('http://www.example.com', 'en')],
+      },
+}
+
 MAIL_CONFIG = {
   # Email server name
   'email_server': 'mailserver.daasi.de',

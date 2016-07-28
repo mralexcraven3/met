88a74d1835deeb708121420ec26c58dc298ebf9d
Andrea Biancini <andrea.biancini@gmail.com>
2016-07-28 09:02:46 +0200

    Modified logger for metadata refresh batch

--- met/metadataparser/refresh_metadata.py
+++ met/metadataparser/refresh_metadata.py
@@ -72,7 +72,7 @@ def refresh(fed_name=None, force_refresh=False, logger=None):
                 log('Updating federation file and metadata_date...', logger, logging.DEBUG)
                 federation.metadata_update = date.today()
                 federation.save(update_fields=['file', 'metadata_update'])
-                log('Federation update time modified with %s' % federation.metadata_update)
+                log('Federation update time modified with %s' % federation.metadata_update, logger, logging.INFO)
 
             log('Updating federation statistics ...', logger, logging.DEBUG)
             federation.compute_new_stats(timestamp=timestamp)
